a78caddbb110bf3b70ca0031da2d71986be65bc2
Tony <tony@gamil.com>
2016-03-10 00:50:01 +0800

    新增数据库帮助类以及各表对象的实体类

---
+++ src/model/City.java
@@ -0,0 +1,32 @@
+package model;
+
+public class City {
+	private int id;
+	private String cityname;
+	private String cityCode;
+	private int provinceId;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCityname() {
+		return cityname;
+	}
+	public void setCityname(String cityname) {
+		this.cityname = cityname;
+	}
+	public String getCityCode() {
+		return cityCode;
+	}
+	public void setCityCode(String cityCode) {
+		this.cityCode = cityCode;
+	}
+	public int getProvinceId() {
+		return provinceId;
+	}
+	public void setProvinceId(int provinceId) {
+		this.provinceId = provinceId;
+	}
+}
---
+++ src/model/County.java
@@ -0,0 +1,33 @@
+package model;
+
+public class County {
+
+	private int id;
+	private String countName;
+	private String countyCode;
+	private int cityId;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCountName() {
+		return countName;
+	}
+	public void setCountName(String countName) {
+		this.countName = countName;
+	}
+	public String getCountyCode() {
+		return countyCode;
+	}
+	public void setCountyCode(String countyCode) {
+		this.countyCode = countyCode;
+	}
+	public int getCityId() {
+		return cityId;
+	}
+	public void setCityId(int cityId) {
+		this.cityId = cityId;
+	}
+}
